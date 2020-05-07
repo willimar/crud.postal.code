9b033356933a95dd4860a6caf645f0e62632c4d9
Willimar Augusto Rocha <willimar@gmail.com>
2020-05-06 23:01:48 -0300

    .

--- docker-build.bat
+++ docker-build.bat
@@ -1,2 +1,9 @@
 docker build -t postalcode-api .
-heroku container:push web -a postalcode-api
+
+heroku login
+heroku container:login
+
+docker tag postalcode-api registry.heroku.com/postalcode-api/web
+docker push registry.heroku.com/postalcode-api/web
+
+heroku container:release web -a postalcode-api
