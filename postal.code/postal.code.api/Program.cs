d4c868eebf7b11601ec02c38753cc01d1267bc21
Willimar Augusto Rocha <willimar@gmail.com>
2020-06-03 21:33:04 -0300

    .

--- postal.code/postal.code.api/Program.cs
+++ postal.code/postal.code.api/Program.cs
@@ -16,8 +16,12 @@ namespace postal.code.api
 
         public static string DataBaseHost { get { lock (_lock) { return GetConfig<string>("MongoDb", "Host"); } } }
         public static int DataBasePort { get { lock (_lock) { return GetConfig<int>("MongoDb", "Port"); } } }
-        internal static string DataBaseUser { get { lock (_lock) { return "atlasUser";/*Cryptographer.Decrypt(GetConfig<string>("MongoDb", "User"), "fodão");*/ } } }
-        internal static string DataBasePws { get { lock (_lock) { return "itsgallus"; /*Cryptographer.Decrypt(GetConfig<string>("MongoDb", "Password"), "fodão");*/ } } }
+        internal static string DataBaseUser { get { lock (_lock) {
+                    Console.WriteLine($"User: {GetConfig<string>("MongoDb", "User")}");
+                    return "atlasUser";/*Cryptographer.Decrypt(GetConfig<string>("MongoDb", "User"), "fodão");*/ } } }
+        internal static string DataBasePws { get { lock (_lock) {
+                    Console.WriteLine($"Password: {GetConfig<string>("MongoDb", "Password")}");
+                    return "itsgallus"; /*Cryptographer.Decrypt(GetConfig<string>("MongoDb", "Password"), "fodão");*/ } } }
         public static string DataBaseAuth { get { lock (_lock) { return GetConfig<string>("MongoDb", "Auth"); } } }
         public static string DataBaseName { get { lock (_lock) { return GetConfig<string>("MongoDb", "DataBase"); } } }
 
